07dc083c4e05f3e923363ab413d27b9fd777877f
Sindre Knudsen Smistad <sks@equinor.com>
2023-11-09 10:43:39 +0100

    feat: order unique constraints and create validation assertions for spelling mistakes

--- Vizgql.Core/Types/SchemaType.cs
+++ Vizgql.Core/Types/SchemaType.cs
@@ -1,4 +1,6 @@
-﻿namespace Vizgql.Core.Types;
+﻿using static Vizgql.Core.Types.ValidationAssertionType;
+
+namespace Vizgql.Core.Types;
 
 public record SchemaType(RootType[] RootTypes)
 {
@@ -6,6 +8,33 @@ public record SchemaType(RootType[] RootTypes)
     {
         var validations = new List<ValidationAssertion>();
 
+        FieldValidations(validations);
+        ConstraintsValidations(validations);
+
+        return validations;
+    }
+
+    private void ConstraintsValidations(List<ValidationAssertion> validations)
+    {
+        var schemaUniqueConstraints = new SchemaUniqueConstraints(this);
+
+        var union = schemaUniqueConstraints.RolesDistances.Union(
+            schemaUniqueConstraints.PoliciesDistances
+        );
+
+        foreach (var rolesDistance in union)
+        {
+            validations.Add(
+                new ValidationAssertion(
+                    $"{rolesDistance.c1} - {rolesDistance.c2}",
+                    ConstraintSpellingMistake
+                )
+            );
+        }
+    }
+
+    private void FieldValidations(List<ValidationAssertion> validations)
+    {
         foreach (var rootType in RootTypes)
         {
             validations.AddRange(rootType.Validate());
@@ -14,7 +43,5 @@ public record SchemaType(RootType[] RootTypes)
                 validations.AddRange(field.Validate(rootType));
             }
         }
-
-        return validations;
     }
 }
--- Vizgql.Core/Types/Types.cs
+++ Vizgql.Core/Types/Types.cs
@@ -6,22 +6,30 @@ public enum ValidationAssertionType
 {
     // A field or root type is missing authorization directive
     MissingAuthorization,
-    
+
     // A field or root type is missing authorization constraints
     MissingAuthorizationConstraints,
-    
+
     // A field is missing authorization directive
-    MissingFieldAuthorization
+    MissingFieldAuthorization,
+
+    // Two constraints are very similar, this might be because of a spelling mistake
+    ConstraintSpellingMistake
 }
 
 public static class ValidationAssertionTypeDescriptions
 {
-    public static string ToString(ValidationAssertionType type)
-        => type switch
+    public static string ToString(ValidationAssertionType type) =>
+        type switch
         {
-            ValidationAssertionType.MissingAuthorization => "The field or root type is missing authorization",
-            ValidationAssertionType.MissingAuthorizationConstraints =>
-                "The field or root type is missing authorization constraints",
-            ValidationAssertionType.MissingFieldAuthorization => "The field is missing authorization",
+            ValidationAssertionType.MissingAuthorization
+                => "The field or root type is missing authorization",
+            ValidationAssertionType.MissingAuthorizationConstraints
+                => "The field or root type is missing authorization constraints",
+            ValidationAssertionType.MissingFieldAuthorization
+                => "The field is missing authorization",
+            ValidationAssertionType.ConstraintSpellingMistake
+                => "The constraint is similar to another constraint, this might be a spelling mistake",
+            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
         };
 }
